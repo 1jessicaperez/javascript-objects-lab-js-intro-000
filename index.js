a623a18e18b42deff8b5ca8413268b647331cbba
Jessica Perez <1jessicaperez@gmail.com>
2020-06-16 18:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   recipes[key] = value
   return recipes
 }
+function deleteFromObjectByKey(recipes, key){
+  delete recipes.key
+  return recipes
+}
